25f9cd385f0bade2f9be4bb43639259df9270c86
ohoncharuk <alex@honcharuk.me>
2017-03-24 21:24:37 +0200

    уау

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,4 @@
 
-host="https://shielded-fortress-95039.herokuapp.com"
-function validateForm(){var email=document.loginform.email.value;var pw=document.loginform.password.value;$.ajax({"url":host+"/login","method":"POST","data":{"email":email,"pw":pw},success:function(data){$(document).ready(function(){$("#user-not-found").hide();$("#bad-password").hide();if(data==="User not found"){$("#user-not-found").show();document.cookie="";return false;}
-if(data==="Bad password"){$("#bad-password").show();document.cookie="";return false;}});if(data!=="User not found"&&data!=="Bad password"){console.log("going to index")
-document.cookie="auth=Ok";window.location.href="./index.html";}}});}
+var host="http://shielded-fortress-95039.herokuapp.com";function validateForm(){var email=document.loginform.email.value;var pw=document.loginform.password.value;$.ajax({"url":host+"/login","method":"POST","data":{"email":email,"pw":pw},success:function(data){$(document).ready(function(){$("#user-not-found").hide();$("#bad-password").hide();if(data==="User not found"){$("#user-not-found").show();localStorage.setItem("auth",false);return false;}
+if(data==="Bad password"){$("#bad-password").show();localStorage.setItem("auth",false);return false;}});if(data!=="User not found"&&data!=="Bad password"){console.log("going to index")
+localStorage.setItem("auth",true);window.location.href="./index.html";}}});}
