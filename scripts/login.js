f458351783f01b442242052d5ae6c16e87981ac6
ohoncharuk <alex@honcharuk.me>
2017-03-24 21:18:28 +0200

    ау

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,5 @@
 
-host='http://localhost:5000'
+host="https://shielded-fortress-95039.herokuapp.com"
 function validateForm(){var email=document.loginform.email.value;var pw=document.loginform.password.value;$.ajax({"url":host+"/login","method":"POST","data":{"email":email,"pw":pw},success:function(data){$(document).ready(function(){$("#user-not-found").hide();$("#bad-password").hide();if(data==="User not found"){$("#user-not-found").show();document.cookie="";return false;}
 if(data==="Bad password"){$("#bad-password").show();document.cookie="";return false;}});if(data!=="User not found"&&data!=="Bad password"){console.log("going to index")
 document.cookie="auth=Ok";window.location.href="./index.html";}}});}
